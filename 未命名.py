01bbbf02300589207af2599cb4c5124749e1daa2
baihuanyu <51882472+baihuanyu@users.noreply.github.com>
2019-06-19 04:06:39 +0800

    Add files via upload

---
+++ 未命名.py
@@ -0,0 +1,31 @@
+# 汉诺塔
+def hanno(n,a,b,c):
+    '''
+    n 代表次数
+    a代表塔A
+    。。
+    。。
+    '''
+    # 假如n等于1是 把a移到c
+    if n == 1:
+        print(a,'-->',c)
+    if n == 2 :
+        print(a,'-->',b)
+        print(a,'-->',c)
+        print(b,'-->',c)
+    # 把n-1个从a移到c
+    def hanno(n-1,a,c,b)
+    print(a,'-->',c)
+    
+    # 把 n-1个盘从b移动到a
+    def hanno(n-1,b,a,c)
+a='A'
+b='B'
+c='C'
+n=3
+def hanno(n,a,b,c)
+
+
+
+
+
